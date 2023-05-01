58c849d5d0a3037f4ba3972d487b364cf70d5dac
Matthew Endicott <matt.endicott@rate.com>
2023-04-30 18:55:59 -0600

    feat(root): 4767110A-E3D5-4BC9-8877-7470B8223B1B

--- src/browser-action/js/browser-action.js
+++ src/browser-action/js/browser-action.js
@@ -20946,45 +20946,6 @@ tree.setInsertionMode("inTableText"),tree.originalInsertionMode=originalInsertio
     jira = jira.replace(/\[([^\]]+)\]/g, "<$1>");
     return jira;
   }
-  var test = `
-h2. QA
-
-{code:javascript}
-  const START = "some-text";
-  const replacementsList: { key: string; value: string }[] = [];
-  let counter = 0;
-{code}
-
-
-* Ensure that there are 3 environments available for testing the form and that they are reading their environment variables correctly
-** dev 
-***  {{pnpm --filter lead-widget-service dev}}
-** qa
-*** {{pnpm --filter lead-widget-service build:qa && pnpm --filter lead-widget-service preview}}
-** production
-*** {{pnpm --filter lead-widget-service build && pnpm --filter lead-widget-service preview}}
-* Ensure that the form can be opened and closed
-** Form Closing Conditions
-*** User clicks the close button
-*** User clicks outside of the form
-* Ensure that if the grins agent is selected for the referrer field the form conditionally displays the active agents from the agent service request {{/agents/names}} for the user to select from
-
--strike me-
-
-
-{code:python}
-  START = "some-python-text"
-  replacementsList = []
-  counter = 0
-{code}
-
-
-{code:none}
-  START = "some-unknownSytax-text"
-  replacementsList = []
-  counter = 0
-{code}`;
-  console.log(toMarkdown(test));
 
   // src/browser-action/ts/mark-magic/index.ts
   var MarkMagic = {
--- src/browser-action/ts/mark-magic/to-markdown.ts
+++ src/browser-action/ts/mark-magic/to-markdown.ts
@@ -61,25 +61,6 @@ export function toMarkdown(jira: string): string {
     return match;
   });
 
-  // -------------
-  // Replace ???
-  // -------------
-  // const map = {
-  //   "??": "cite",
-  //   "-": "del",
-  //   "+": "ins",
-  //   "^": "sup",
-  //   "~": "sub",
-  // };
-
-  // jira = jira.replace(
-  //   new RegExp("(" + Object.keys(map).join("|") + ")(.*?)\\1", "g"),
-  //   (_, from: keyof typeof map, content) => {
-  //     const to = map[from];
-  //     return `<${to}>${content}</${to}>`;
-  //   }
-  // );
-
   // ---------------------------------
   // Replace inline code characters
   // ---------------------------------
@@ -97,44 +78,3 @@ export function toMarkdown(jira: string): string {
 
   return jira;
 }
-
-const test = `
-h2. QA
-
-{code:javascript}
-  const START = "some-text";
-  const replacementsList: { key: string; value: string }[] = [];
-  let counter = 0;
-{code}
-
-
-* Ensure that there are 3 environments available for testing the form and that they are reading their environment variables correctly
-** dev 
-***  {{pnpm --filter lead-widget-service dev}}
-** qa
-*** {{pnpm --filter lead-widget-service build:qa && pnpm --filter lead-widget-service preview}}
-** production
-*** {{pnpm --filter lead-widget-service build && pnpm --filter lead-widget-service preview}}
-* Ensure that the form can be opened and closed
-** Form Closing Conditions
-*** User clicks the close button
-*** User clicks outside of the form
-* Ensure that if the grins agent is selected for the referrer field the form conditionally displays the active agents from the agent service request {{/agents/names}} for the user to select from
-
--strike me-
-
-
-{code:python}
-  START = "some-python-text"
-  replacementsList = []
-  counter = 0
-{code}
-
-
-{code:none}
-  START = "some-unknownSytax-text"
-  replacementsList = []
-  counter = 0
-{code}`;
-
-console.log(toMarkdown(test));
