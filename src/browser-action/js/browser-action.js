77ef5d6c878b85306b5c4e0a256ffd7ec03c81b0
Matthew Endicott <matt.endicott@rate.com>
2023-04-30 18:55:22 -0600

    feat(root): 8DA9C1F4-0387-4721-98E2-021A8415D049

--- src/browser-action/js/browser-action.js
+++ src/browser-action/js/browser-action.js
@@ -20915,45 +20915,76 @@ tree.setInsertionMode("inTableText"),tree.originalInsertionMode=originalInsertio
   }
 
   // src/browser-action/ts/mark-magic/to-markdown.ts
-  function toMarkdown(input) {
-    input = input.replace(/^h([0-6])\.(.*)$/gm, (match, level, content) => {
-      return Array(parseInt(level) + 1).join("#") + content;
-    });
-    input = input.replace(/([*_])(.*)\1/g, (match, wrapper, content) => {
-      const to = wrapper === "*" ? "**" : "*";
-      return to + content + to;
-    });
-    input = input.replace(/\{\{([^}]+)\}\}/g, "`$1`");
-    input = input.replace(/\?\?((?:.[^?]|[^?].)+)\?\?/g, "<cite>$1</cite>");
-    input = input.replace(/\+([^+]*)\+/g, "<ins>$1</ins>");
-    input = input.replace(/\^([^^]*)\^/g, "<sup>$1</sup>");
-    input = input.replace(/~([^~]*)~/g, "<sub>$1</sub>");
-    input = input.replace(/-([^-]*)-/g, "-$1-");
-    input = input.replace(/\{code(:([a-z]+))?\}([^]*)\{code\}/gm, "```$2$3```");
-    input = input.replace(/\[(.+?)\|(.+)\]/g, "[$1]($2)");
-    input = input.replace(/\[(.+?)\]([^\(]*)/g, "<$1>$2");
-    input = input.replace(/{noformat}/g, "```");
-    const lines = input.split(/\r?\n/gm);
-    const lines_to_remove = [];
-    for (let i = 0; i < lines.length; i++) {
-      let line_content = lines[i];
-      const seperators = line_content.match(/\|\|/g);
-      if (seperators !== null) {
-        lines[i] = lines[i].replace(/\|\|/g, "|");
-        let header_line = "";
-        for (let j = 0; j < seperators.length - 1; j++) {
-          header_line += "|---";
+  function toMarkdown(jira) {
+    const matchCodeBlockWithSyntaxRegex = /\{code(?:\:(\w+))?\}((?:\n|.)+?)\{code\}/g;
+    jira = jira.replace(
+      matchCodeBlockWithSyntaxRegex,
+      (_codeBlock, codeBlockSyntaxType, codeBlockContent) => {
+        let code = "```";
+        if (codeBlockSyntaxType) {
+          code += codeBlockSyntaxType;
         }
-        header_line += "|";
-        lines.splice(i + 1, 0, header_line);
+        code += codeBlockContent + "```";
+        return code;
       }
-    }
-    input = "";
-    for (let i = 0; i < lines.length; i++) {
-      input += lines[i] + "\n";
-    }
-    return input;
+    );
+    jira = jira.replace(/^h([1-6])\.(.*?)$/gm, (_, level, content) => {
+      return "#".repeat(parseInt(level)) + content;
+    });
+    jira = jira.replace(/\*(\w[\w|" "]+\w)\*/g, "**$1**");
+    jira = jira.replace(/_(\w[\w|" "]+\w)_/g, "*$1*");
+    jira = jira.replace(/\s-(\w[\w|" "]+\w)-\s/g, (match, groupOne) => {
+      return match.replaceAll("-", "~~");
+    });
+    jira = jira.replace(/(^\*+\s)(.+)/gm, (match, groupOne, groupTwo) => {
+      const prefix = "	".repeat(groupOne.length - 2) + "- ";
+      match = prefix + groupTwo;
+      return match;
+    });
+    jira = jira.replace(/\{\{([^}]+)\}\}/g, "`$1`");
+    jira = jira.replace(/\[([^\]]+)\|([^\]]+)\]/g, "[$1]($2)");
+    jira = jira.replace(/\[([^\]]+)\]/g, "<$1>");
+    return jira;
   }
+  var test = `
+h2. QA
+
+{code:javascript}
+  const START = "some-text";
+  const replacementsList: { key: string; value: string }[] = [];
+  let counter = 0;
+{code}
+
+
+* Ensure that there are 3 environments available for testing the form and that they are reading their environment variables correctly
+** dev 
+***  {{pnpm --filter lead-widget-service dev}}
+** qa
+*** {{pnpm --filter lead-widget-service build:qa && pnpm --filter lead-widget-service preview}}
+** production
+*** {{pnpm --filter lead-widget-service build && pnpm --filter lead-widget-service preview}}
+* Ensure that the form can be opened and closed
+** Form Closing Conditions
+*** User clicks the close button
+*** User clicks outside of the form
+* Ensure that if the grins agent is selected for the referrer field the form conditionally displays the active agents from the agent service request {{/agents/names}} for the user to select from
+
+-strike me-
+
+
+{code:python}
+  START = "some-python-text"
+  replacementsList = []
+  counter = 0
+{code}
+
+
+{code:none}
+  START = "some-unknownSytax-text"
+  replacementsList = []
+  counter = 0
+{code}`;
+  console.log(toMarkdown(test));
 
   // src/browser-action/ts/mark-magic/index.ts
   var MarkMagic = {
--- src/browser-action/ts/mark-magic/to-markdown.ts
+++ src/browser-action/ts/mark-magic/to-markdown.ts
@@ -1,61 +1,140 @@
+import { getSupportedName } from "./utils";
+
 /**
- * Takes Jira markup and converts it to Markdown.
+ * Converts Jira Markup to GitHub Flavored Markdown
  *
- * @param input - Jira markup text
- * @returns Markdown formatted text
+ * @param {string} jira - Jira markup text
+ * @returns {string} Markdown formatted text
  */
-export function toMarkdown(input: string): string {
-  input = input.replace(/^h([0-6])\.(.*)$/gm, (match, level, content) => {
-    return Array(parseInt(level) + 1).join("#") + content;
-  });
+export function toMarkdown(jira: string): string {
+  // ---------------------
+  // Replace code blocks
+  // ---------------------
+  const matchCodeBlockWithSyntaxRegex =
+    /\{code(?:\:(\w+))?\}((?:\n|.)+?)\{code\}/g;
+  jira = jira.replace(
+    matchCodeBlockWithSyntaxRegex,
+    (_codeBlock, codeBlockSyntaxType, codeBlockContent) => {
+      let code = "```";
 
-  input = input.replace(/([*_])(.*)\1/g, (match, wrapper, content) => {
-    const to = wrapper === "*" ? "**" : "*";
-    return to + content + to;
-  });
-
-  input = input.replace(/\{\{([^}]+)\}\}/g, "`$1`");
-  input = input.replace(/\?\?((?:.[^?]|[^?].)+)\?\?/g, "<cite>$1</cite>");
-  input = input.replace(/\+([^+]*)\+/g, "<ins>$1</ins>");
-  input = input.replace(/\^([^^]*)\^/g, "<sup>$1</sup>");
-  input = input.replace(/~([^~]*)~/g, "<sub>$1</sub>");
-  input = input.replace(/-([^-]*)-/g, "-$1-");
-
-  input = input.replace(/\{code(:([a-z]+))?\}([^]*)\{code\}/gm, "```$2$3```");
-
-  input = input.replace(/\[(.+?)\|(.+)\]/g, "[$1]($2)");
-  input = input.replace(/\[(.+?)\]([^\(]*)/g, "<$1>$2");
-
-  input = input.replace(/{noformat}/g, "```");
-
-  // Convert header rows of tables by splitting input on lines
-  const lines = input.split(/\r?\n/gm);
-  const lines_to_remove: number[] = [];
-  for (let i = 0; i < lines.length; i++) {
-    let line_content = lines[i];
-
-    const seperators = line_content.match(/\|\|/g);
-    if (seperators !== null) {
-      lines[i] = lines[i].replace(/\|\|/g, "|");
-
-      // Add a new line to mark the header in Markdown,
-      // we require that at least 3 -'s are between each |
-      let header_line = "";
-      for (let j = 0; j < seperators.length - 1; j++) {
-        header_line += "|---";
+      if (codeBlockSyntaxType) {
+        code += codeBlockSyntaxType;
       }
 
-      header_line += "|";
+      code += codeBlockContent + "```";
 
-      lines.splice(i + 1, 0, header_line);
+      return code;
     }
-  }
+  );
 
-  // Join the split lines back
-  input = "";
-  for (let i = 0; i < lines.length; i++) {
-    input += lines[i] + "\n";
-  }
+  // ---------------------
+  // Replace headings
+  // ---------------------
+  jira = jira.replace(/^h([1-6])\.(.*?)$/gm, (_, level, content) => {
+    return "#".repeat(parseInt(level)) + content;
+  });
 
-  return input;
+  // --------------------------------------------------------------------------------------------------------
+  // Replace bold characters (!!! Bold replaced first to avoid conflicts with post replacement italics !!!)
+  // // --------------------------------------------------------------------------------------------------------
+  jira = jira.replace(/\*(\w[\w|" "]+\w)\*/g, "**$1**");
+
+  // ---------------------------
+  // Replace italics characters
+  // ---------------------------
+  jira = jira.replace(/_(\w[\w|" "]+\w)_/g, "*$1*");
+
+  // ----------------------------------
+  // Replace strikethrough characters
+  // ----------------------------------
+  jira = jira.replace(/\s-(\w[\w|" "]+\w)-\s/g, (match, groupOne) => {
+    return match.replaceAll("-", "~~");
+  });
+
+  // ----------------------------
+  // Replace lists with bullets
+  // ----------------------------
+  jira = jira.replace(/(^\*+\s)(.+)/gm, (match, groupOne, groupTwo) => {
+    const prefix = "\t".repeat(groupOne.length - 2) + "- ";
+    match = prefix + groupTwo;
+
+    return match;
+  });
+
+  // -------------
+  // Replace ???
+  // -------------
+  // const map = {
+  //   "??": "cite",
+  //   "-": "del",
+  //   "+": "ins",
+  //   "^": "sup",
+  //   "~": "sub",
+  // };
+
+  // jira = jira.replace(
+  //   new RegExp("(" + Object.keys(map).join("|") + ")(.*?)\\1", "g"),
+  //   (_, from: keyof typeof map, content) => {
+  //     const to = map[from];
+  //     return `<${to}>${content}</${to}>`;
+  //   }
+  // );
+
+  // ---------------------------------
+  // Replace inline code characters
+  // ---------------------------------
+  jira = jira.replace(/\{\{([^}]+)\}\}/g, "`$1`");
+
+  // ---------------
+  // Replace links
+  // ---------------
+  jira = jira.replace(/\[([^\]]+)\|([^\]]+)\]/g, "[$1]($2)");
+
+  // ----------------------------
+  // Replace [link] with <link>
+  // ----------------------------
+  jira = jira.replace(/\[([^\]]+)\]/g, "<$1>");
+
+  return jira;
 }
+
+const test = `
+h2. QA
+
+{code:javascript}
+  const START = "some-text";
+  const replacementsList: { key: string; value: string }[] = [];
+  let counter = 0;
+{code}
+
+
+* Ensure that there are 3 environments available for testing the form and that they are reading their environment variables correctly
+** dev 
+***  {{pnpm --filter lead-widget-service dev}}
+** qa
+*** {{pnpm --filter lead-widget-service build:qa && pnpm --filter lead-widget-service preview}}
+** production
+*** {{pnpm --filter lead-widget-service build && pnpm --filter lead-widget-service preview}}
+* Ensure that the form can be opened and closed
+** Form Closing Conditions
+*** User clicks the close button
+*** User clicks outside of the form
+* Ensure that if the grins agent is selected for the referrer field the form conditionally displays the active agents from the agent service request {{/agents/names}} for the user to select from
+
+-strike me-
+
+
+{code:python}
+  START = "some-python-text"
+  replacementsList = []
+  counter = 0
+{code}
+
+
+{code:none}
+  START = "some-unknownSytax-text"
+  replacementsList = []
+  counter = 0
+{code}`;
+
+console.log(toMarkdown(test));
