ce79fa9afaeb3605d422ac1aabdc883d28791226
Matthew Endicott <matt.endicott@rate.com>
2023-08-15 13:15:01 -0600

    feat(root): 48F791AE-2B4B-49C3-9461-28A8EBC357C6

--- src/browser-action/ts/mark-magic/to-markdown.ts
+++ src/browser-action/ts/mark-magic/to-markdown.ts
@@ -1,5 +1,3 @@
-import { getSupportedName } from "./utils";
-
 /**
  * Converts Jira Markup to GitHub Flavored Markdown
  *
@@ -47,7 +45,7 @@ export function toMarkdown(jira: string): string {
   // ----------------------------------
   // Replace strikethrough characters
   // ----------------------------------
-  jira = jira.replace(/\s-(\w[\w|" "]+\w)-\s/g, (match, groupOne) => {
+  jira = jira.replace(/\s-(\w[\w|" "]+\w)-\s/g, (match) => {
     return match.replaceAll("-", "~~");
   });
 
@@ -76,5 +74,74 @@ export function toMarkdown(jira: string): string {
   // ----------------------------
   jira = jira.replace(/\[([^\]]+)\]/g, "<$1>");
 
+  // ---------------------------------
+  // Replace table header identifiers
+  // ---------------------------------
+  jira = jira.replace(/\|\|/g, "|");
+
+  // ----------------------------
+  // Replace table header cells
+  // ----------------------------
+  /*
+  ||heading 1||heading 2||heading 3||
+  |col A1|col A2|col A3|
+  |col B1|col B2|col B3|
+  */
+
+  /*
+  ||heading 1||heading 2||heading 3||
+  |col A1|col A2|col A3|
+  |col B1|col B2|col B3|
+  */
+
+  // TODO: Add logic
+
   return jira;
 }
+
+const test = `h2. QA
+
+||heading 1||heading 2||heading 3||
+|col A1|col A2|col A3|
+|col B1|col B2|col B3|
+
+{code:javascript}
+  const START = "some-text";
+  const replacementsList: { key: string; value: string }[] = [];
+  let counter = 0;
+{code}
+
+
+* Ensure that there are 3 environments available for testing the form and that they are reading their environment variables correctly
+** dev
+***  {{pnpm --filter lead-widget-service dev}}
+** qa
+*** {{pnpm --filter lead-widget-service build:qa && pnpm --filter lead-widget-service preview}}
+** production
+*** {{pnpm --filter lead-widget-service build && pnpm --filter lead-widget-service preview}}
+* Ensure that the form can be opened and closed
+** Form Closing Conditions
+*** User clicks the close button
+*** User clicks outside of the form
+* Ensure that if the grins agent is selected for the referrer field the form conditionally displays the active agents from the agent service request {{/agents/names}} for the user to select from
+
+-strike me-
+
+_italic me_
+
+*bold me*
+
+{code:python}
+  START = "some-python-text"
+  replacementsList = []
+  counter = 0
+{code}
+
+
+{code:none}
+  START = "some-unknownSytax-text"
+  replacementsList = []
+  counter = 0
+{code}`;
+
+console.log(toMarkdown(test));
